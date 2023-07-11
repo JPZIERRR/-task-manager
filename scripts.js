0a2126037d7246d310a2fda41daf149c5f47dfec
JPZIERRR <joaofornaz_ier@hotmail.com>
2023-07-11 16:37:18 -0300

    last-commit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 'use strict';
+
+/* ICONS */
+const iconPen =
+  '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAghJREFUSEvl1snLTmEYx/HPG0sLykyZk6TsDGXa+QOsFGWIsGGHDdmwEUpYmBb8EVZmsZKhSAoZQ8RCidCl6+g4nvk9T+/CvXnuznB9z+93X8MzYIjWwBBx9Qs8HPNwH98aiesHeAouYDYeYiWeVeF1gwN6HZNKoBdYgqdleJ3g6biKiQm4jGW5D8WL8LqA1wUO6A2My8AHsAv7sTOvncH6OsGzcA1jM+gtLCzZehML8BUj8D3uDVZxVWnB24c9FcVnsa4OxZG1lzA+gx3BqlJiXcSKvPcyVcfv79Wr4oBG8lTPdGom2OR+ZPWcVFqcaSTQ7hLoKLaVlEYpPRlsHYfSSKTRGegwdpSC7s2zjUsN67cXq9tBo2xCfaw3Wbd/NY1eGsjctLdQehxbS4Gq0LD3cdXebsEBjY40Kl+sQrfjUElpW2gnWd0N9B0Wt1PayRlXoaexoWRXWWlAl+Y0auXwn3vN6rgRdCN+5ptbcCz3XUNbWf0cRRM4iU0laNRo1GqsgMYEetCRzNJDjRRH03+Uz5zD2iZK36e9XUObKd6MEwmejzu5j5F2KvcfsBz3ulXaKrnOYzXeZi8egzU4WBe0meJXmIBP+JL74kM/5sQpXOhV8D/TaUaLOgxo2Hu3Z1qL5JqZtTgMn3Elx1/M1tv4UQe0mdXTMDKTqjZQ9YN7/SMwaOH/H/gX8oxyHyld2yAAAAAASUVORK5CYII="/>';
+
+const iconTrash =
+  '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARpJREFUSEvtlkGuAUEQhj+xsuIIjiAu8DgDCwvCDSRij7UFRyBsJJwBFxBHcARWb4dUYqQyjNY9k4xF13K6q77uv6qnKkNKlkmJiw04C/SBXMRh/4ExcP3mMjbgKdA1BJ0AvSTBbWD2TUCgAaxMe8M3rgBbk5PjehXYBb4/Ay4CHccbmdwkVaeoG2vnElAwRTOsn4Hjuz2fqlry8RcTvAekbl7Mg7UkYalHj0WRLpwCkTR4KgMVJBGpg7QMAR1cOHIo+S528+C4Ve2l1gr64grU8O/4WRc2TUImCDHp1zIKaZur0UhPMIlI7dIhncBroOZCUz4boG7752oCi5jgFrC0Bcv+8qMF5i0PcAFE5kOUn81Ab8n+vD018B1Y2lofc0pDAwAAAABJRU5ErkJggg=="/>';
+
+///////////////////////////////////////////
+
+/* Seletores */
 const input = document.querySelector('.input');
 const buttonAdd = document.querySelector('.add-task');
-const error = document.querySelector('.empty-input-error');
 const listContainer = document.querySelector('.list-task');
 
 /* Janela de edição */
@@ -20,12 +30,13 @@ const idGenerator = function () {
 /* Eventos */
 input.addEventListener('keypress', e => {
   if (e.keyCode === 13) {
-    e.preventDefault;
     const task = {
       name: input.value,
       id: idGenerator(),
     };
-
+    if (task.name === '') {
+      return;
+    }
     addTask(task);
   }
 });
@@ -35,7 +46,9 @@ buttonAdd.addEventListener('click', () => {
     name: input.value,
     id: idGenerator(),
   };
-
+  if (task.name === '') {
+    return;
+  }
   addTask(task);
 });
 
@@ -52,6 +65,10 @@ updateButton.addEventListener('click', e => {
     name: inputNewTask.value,
     id: taskId,
   };
+
+  if (task.name === '') {
+    return;
+  }
 
   const currentTask = document.getElementById(`${taskId}`);
 
@@ -73,6 +90,10 @@ inputNewTask.addEventListener('keypress', e => {
       id: taskId,
     };
 
+    if (task.name === '') {
+      return;
+    }
+
     const currentTask = document.getElementById(`${taskId}`);
 
     if (currentTask) {
@@ -93,6 +114,7 @@ const addTask = function (task) {
 const createLi = function (task) {
   const li = document.createElement('li');
   li.id = task.id;
+  li.classList.add('list');
 
   const span = document.createElement('span');
   span.classList.add('task-text');
@@ -102,12 +124,12 @@ const createLi = function (task) {
 
   const editButton = document.createElement('button');
   editButton.classList.add('edit-btn');
-  editButton.innerHTML = 'edit';
+  editButton.innerHTML = iconPen;
   editButton.setAttribute(`onClick`, `edit(${task.id})`);
 
   const deleteButton = document.createElement('button');
   deleteButton.classList.add('delete-btn');
-  deleteButton.innerHTML = 'delete';
+  deleteButton.innerHTML = iconTrash;
   deleteButton.setAttribute(`onClick`, `delet(${task.id})`);
 
   div.appendChild(editButton);
@@ -120,10 +142,9 @@ const createLi = function (task) {
 
 const edit = function (idTask) {
   const li = document.getElementById(`${idTask}`);
-  const span = document.querySelector('.task-text');
   if (li) {
     editIdTask.innerHTML = `#${idTask}`;
-    inputNewTask.value = span.innerText;
+    inputNewTask.value = li.innerText;
     changeWindow();
   }
 };
